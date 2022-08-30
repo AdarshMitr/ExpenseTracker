53c4a600c93d3e47f125f20072a6eff60d03064b
AdarshMitr <adarshmitr@gmail.com>
2022-08-30 20:21:50 +0530

    added styles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,13 @@
+
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 function ExpenseItem(props) {
-const expenseDate = new Date(2021,2,15);
-const expenseTitle='Bike Insurance';
-const expenseAmount= 3000;
-
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title} </h2>
-                <div className='expense-item__price'>{props.amount}</div>
+                <div className='expense-item__price'>₹ {props.amount}</div>
             </div>
         </div>
         );
