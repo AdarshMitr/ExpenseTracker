5e1177dda0bfb76e407ba788d0a96385bcec0af3
AdarshMitr <adarshmitr@gmail.com>
2022-09-04 13:37:10 +0530

    finalizing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import NewExpense from "./components/NewExpense/NewExpense";
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Books',
+    amount: 2000,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New Monitor', amount: 5500, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Bike Insurance',
+    amount: 3000,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-import Expenses from "./components/Expenses/Expenses";
-import ExpenseItem from "./components/Expenses/ExpenseItem";
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Computer Accessories',
-      amount: 5000,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 25000, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Bike Insurance',
-      amount: 3000,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 2800,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-    
- 
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <NewExpense/>
-      <Expenses items={expenses}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
